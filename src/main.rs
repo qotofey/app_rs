126f3e333234ebed0abe18fa80d32eda56250167
Timofei Shufletiuk <ext-timofey.shufletyuk@sbermarket.ru>
2022-07-31 23:57:03 +0800

    update README

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
 fn main() {
     println!("Hello, Rust!");
+    let max:f32 = std::f32::MAX;
+    let min:f32 = std::f32::MIN;
+    println!("max f32 - {}", max);
+    println!("min f32 - {}", min);
+    let max:f64 = std::f64::MAX;
+    let min:f64 = std::f64::MIN;
+    println!("max f64 - {}", max);
+    println!("min f64 - {}", min);
 }
